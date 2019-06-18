1783e81b3cfc2c95dd2f4b5aa64f62fea086040f
rahul02468 <rahul123321rg@gmail.com>
2019-06-18 17:32:36 +0530

    eventsonhomepage

--- src/components/home/eventshowcase.vue
+++ src/components/home/eventshowcase.vue
@@ -9,7 +9,96 @@
                <router-link to="/events" flat color="#4C4A78" class="ma-0 google-font" style="border-radius:5px;text-transform: capitalize;text-decoration:none;color:#4C4A78">See More</router-link>             
             </v-flex> 
         </v-layout>
-        <p class="google-font" style="font-size:170%; display:block;">Past Events</p>
+        <p class="google-font" style="font-size:170%; display:block;color:#0277bd">Past Events</p>
+        <v-layout wrap align-start justify-center row fill-height class="hidden-sm-and-down mb-4">
+            <v-flex xs12 class="text-xs-center" v-if="showLoader">
+                <v-progress-circular
+                    :size="50"
+                    color="blue"
+                    indeterminate
+                ></v-progress-circular>
+            </v-flex>
+            <v-flex xs12 sm6 md3 lg3 v-for="(item,i) in eventsData" :key="i">
+                <v-card 
+                    flat
+                    class="ma-1 pa-1 my-0" 
+                    style="border: 1px solid #e0e0e0;min-height:160px;border-radius:7px">
+
+                    <v-card-title class="mb-0">
+                        <div>
+                            <p class="google-font mb-0" style="font-size:130%">{{ item.name | summery(25) }}</p>
+                            <p class="google-font mt-1 mb-0" style="font-size:110%">
+                                <v-icon small>insert_invitation</v-icon>
+                                {{item.local_date | dateFilter}}
+                            </p>
+                            <p class="google-font mt-0 mb-0" style="font-size:110%">
+                                <v-icon small>watch_later</v-icon>
+                                {{item.local_time}}
+                            </p>
+                            <p class="google-font mt-0 mb-0" style="font-size:110%">
+                                <v-icon small>map</v-icon>
+                                {{item.venue.name | summery(30)}}
+                            </p>
+                        </div>
+                    </v-card-title>
+
+                   
+                     <v-card-actions class="mt-0">
+                        <v-spacer></v-spacer>
+                        <v-btn flat color="#4C4A78" :href="item.link" target="_blank" class="mb-0 ml-0 mt-0 google-font" style="border-radius:7px;text-transform: capitalize;">See More</v-btn> 
+                    </v-card-actions>
+                    
+                </v-card>
+            </v-flex>
+        </v-layout>
+
+        <v-layout wrap align-center justify-center row fill-height class="hidden-md-and-up mb-3">
+            <v-flex xs12 class="text-xs-center" v-if="showLoader">
+                <v-progress-circular
+                    :size="50"
+                    color="blue"
+                    indeterminate
+                ></v-progress-circular>
+            </v-flex>
+
+            <v-flex xs12>
+                <v-slide-y-reverse-transition>
+                    <v-list two-line subheader v-show="showData">
+                        <v-list-tile
+                            v-for="(item,i) in eventsData" :key="i"
+                            avatar
+                            style="border-color:#e0e0e0;border-width: 1px;border-style: solid;border-top:0; border-left:0; border-right:0; border-bottom:1"
+                        >
+                            <v-list-tile-avatar>
+                                <v-icon>view_compact</v-icon>
+                            </v-list-tile-avatar>
+
+                            <v-list-tile-content>
+                                <v-list-tile-title class="google-font">{{ item.name }}</v-list-tile-title>
+                                <v-list-tile-sub-title class="google-font">{{ item.local_date |dateFilter }} | {{ item.local_time }}</v-list-tile-sub-title>
+                            </v-list-tile-content>
+
+                            <v-list-tile-action>
+                                <v-btn icon ripple :href="item.link"
+                                target="_blank">
+                                    <v-icon color="grey lighten-1">arrow_forward</v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
+                            
+                        </v-list-tile>
+                        
+
+                    </v-list>
+                </v-slide-y-reverse-transition>
+            </v-flex>
+        </v-layout>
+
+
+
+        <!-- upcoming parts starts -->
+        <!-- upcoming parts starts -->
+        <!-- upcoming parts starts -->
+        <p class="google-font" style="font-size:170%; display:block;color:#0277bd">Upcoming Events</p>
         <v-layout wrap align-start justify-center row fill-height class="hidden-sm-and-down mb-4">
             <v-flex xs12 class="text-xs-center" v-if="showLoader">
                 <v-progress-circular
@@ -94,6 +183,12 @@
         </v-layout>
 
     </v-container>
+
+
+
+
+    
+
 </template>
 
 <script>

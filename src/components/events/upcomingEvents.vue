4f3097525a1398e4388d52860ec9e1d499a63a4c
rahul02468 <rahul123321rg@gmail.com>
2019-06-18 20:24:35 +0530

    banner

--- src/components/events/upcomingEvents.vue
+++ src/components/events/upcomingEvents.vue
@@ -1,5 +1,76 @@
 <template :style="{'background-image':'url('+require('@/assets/img/svg/bg.svg')+')'}" style="background-position:right">
     <v-container class="pa-0 ">
+
+        <v-layout wrap align-center justify-center row fill-height class="mt-0 mb-0" >
+           <v-flex xs12 md12 lg12 class="pa-2 mb-0">
+               <p class="google-font mb-0" style="font-size:170%;color:#0277bd">Our Feature Event & Meetup</p>     
+            </v-flex> 
+        </v-layout>
+
+        <v-layout  wrap align-center justify-center row fill-height class="mt-2 elevation-2 white" style="border:1px solid #e0e0e0;border-radius:5px;margin-bottom: 30px;"  v-if="eventDetails.IsReady">
+             <v-flex xs12 v-if="showLoader">
+                <v-progress-circular
+                     :size="50"
+                     color="blue"
+                     indeterminate
+                 ></v-progress-circular>
+             </v-flex>
+             <v-flex xs12 sm4 md3 lg3 class="pa-4">
+                 <v-img
+                     :src="eventDetails.EventImage"
+                     :lazy-src="eventDetails.EventImage"
+                     width="100%">
+                     <v-layout
+                         slot="placeholder"
+                         fill-height
+                         align-center
+                         justify-center
+                         ma-0
+                     >
+                         <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                     </v-layout>
+                 </v-img>
+             </v-flex>
+            <v-flex xs12 sm8 md9 lg9 class="pa-2 py-4 px-3" >
+                 <p class="google-font mb-0" style="font-size:150%;color:rgb(2, 119, 189)">{{eventDetails.FeatureEventName}}</p>
+                 <span class="google-font mt-1 mb-0 grey--text"  style="font-size:105%">
+                     <v-icon small>insert_invitation</v-icon>
+                     {{eventDetails.EventDate.Date +'/'+ eventDetails.EventDate.Month +'/'+ eventDetails.EventDate.Year}} 
+                     &nbsp;
+                     <v-icon small>watch_later</v-icon>
+                     {{eventDetails.EventTime.StartTime +' - '+ eventDetails.EventTime.EndTime}}
+                     &nbsp;
+                     <v-icon small>map</v-icon>
+                     {{eventDetails.EventVenue | summery(50)}} <a :href="eventDetails.EventVenueMapLink" target="_blank">(Map)</a>
+                 </span>    
+                <p class="google-font mt-2 mb-1" style="font-size:115%;color:#757575">
+                    {{eventDetails.EventDescription}}
+                </p>
+                
+                 <v-btn color="#1a73e8" v-if="eventDetails.RegistrationLink.length>0" :href="eventDetails.RegistrationLink" target="_blank" rel="noopener" class="ma-0 elevation-0 my-2" dark style="text-transform: capitalize;border-radius:5px;"> 
+                     Registration Link
+                 </v-btn>
+                 &nbsp;
+
+                 <v-tooltip top slot="activator">
+                     <v-btn flat :href="eventDetails.EventWebsite" target="_blank" rel="noopener" icon color="#616161" class="ma-0 elevation-0" slot="activator" style="text-transform: capitalize;border-radius:5px;"> 
+                         <v-icon>language</v-icon>
+                     </v-btn>
+                     <span>See {{eventDetails.FeatureEventName}} Website</span>
+                 </v-tooltip>
+
+                 <v-tooltip top slot="activator">
+                     <v-btn flat  :href="eventDetails.MeetupLink" target="_blank" rel="noopener" icon color="#616161" class="ma-0 elevation-0" slot="activator" style="text-transform: capitalize;border-radius:5px;"> 
+                         <v-icon>fab fa-meetup</v-icon>
+                     </v-btn>
+                     <span>See {{eventDetails.FeatureEventName}} Meetup</span>
+                 </v-tooltip>
+            
+             </v-flex> 
+        </v-layout>
+
+
+
         <v-layout wrap align-center justify-center row fill-height class="mt-0 mb-0" >
            <v-flex xs12 md12 lg12 class="pa-2 mb-0">
                <p class="google-font mb-0" style="font-size:170%;color:#0277bd">Upcoming Events</p>
